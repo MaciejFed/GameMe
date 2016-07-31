2c7051abee7aec38150384c24763b87dd6f79999
MaciejFed <mfedorowiat@gmail.com>
2016-07-31 21:19:28 +0200

    Correct Docker DB Name

--- runApp.cmd
+++ runApp.cmd
@@ -5,7 +5,7 @@ docker rm -f GameMeUI
 echo building the GameMeDB...
 
 cd db
-docker build -t maciejfed/db .
+docker build -t maciejfed/gameme-db .
 
 echo building the GameMeService...
 
@@ -22,7 +22,7 @@ docker build -t maciejfed/gameme-ui .
 
 echo starting dockers...
 
-docker run --name GameMeDB -d -p 27017:27017 maciejfed/db
+docker run --name GameMeDB -d -p 27017:27017 maciejfed/gameme-db
 docker run --name GameMeService -d -p 8080:8080 --link GameMeDB maciejfed/gameme-service
 docker run --name GameMeUI -d -p 80:80 --link GameMeService maciejfed/gameme-ui
 
