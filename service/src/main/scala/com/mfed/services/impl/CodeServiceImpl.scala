f160d12ac67f4ae9fa9461d757175593184da6c7
Magdalena Gargas <mgargas@x-formation.com>
2016-12-09 19:23:28 +0100

    Create CodeRunner; Add raw CodeServiceImpl

--- service/src/main/scala/com/mfed/services/impl/CodeServiceImpl.scala
+++ service/src/main/scala/com/mfed/services/impl/CodeServiceImpl.scala
@@ -1,11 +1,38 @@
 package com.mfed.services.impl
 
+import java.io.File
+import java.net.{URL, URLClassLoader}
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import javax.tools.{ToolProvider, JavaCompiler}
+
+import com.mfed.GameMeApplication
 import com.mfed.services.CodeService
 import org.springframework.stereotype.Service
 
 @Service
 class CodeServiceImpl extends CodeService{
-  override def compile(code: String): String = ???
+  override def compile(code: String): String = {
+    val root: File = prepFile
+    val sourceFile: File = new File(root, "Test.java")
+    sourceFile.getParentFile.mkdirs
+    Files.write(sourceFile.toPath, code.getBytes(StandardCharsets.UTF_8))
 
-  override def run(code: String): List[String] = ???
+    val compiler: JavaCompiler = ToolProvider.getSystemJavaCompiler
+
+    compiler.run(null, null, null, sourceFile.getPath).toString
+  }
+
+  override def run(code: String): List[String] = {
+    val root = prepFile
+    val classLoader: URLClassLoader = URLClassLoader.newInstance(Array[URL](root.toURI.toURL))
+    val cls: Class[_] = Class.forName("Test", true, classLoader)
+    val instance: Any = cls.newInstance
+    List(instance.toString)
+  }
+
+  def prepFile: File = {
+    val path = classOf[GameMeApplication].getProtectionDomain.getCodeSource.getLocation.getPath.split("out")(0)
+    new File(path)
+  }
 }
