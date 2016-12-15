15886752ce9f9f68d11f4d90b98711da942c9b4a
Magdalena Gargas <mgargas@x-formation.com>
2016-12-15 23:47:59 +0100

    Create_CodeRunner; Compile code, run code - for path of length 1 and only first request

--- service/src/main/scala/com/mfed/api/controllers/CodeController.scala
+++ service/src/main/scala/com/mfed/api/controllers/CodeController.scala
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, ResponseEntity}
 import org.springframework.web.bind.annotation._
 
-class CodeController {
+import scala.collection.JavaConversions._
 
-  @RestController
-  @RequestMapping(Array("/code"))
-  class GameMapController {
+@RestController
+@RequestMapping(Array("/code"))
+class CodeController {
 
     @Autowired
     private val codeService: CodeService = null
@@ -25,10 +25,12 @@ class CodeController {
     }
 
     @RequestMapping(value = Array("/{levelNumber}/run"), method = Array(RequestMethod.POST))
-    def run(@PathVariable levelNumber: Int, @RequestBody code: String): ResponseEntity[List[String]] = {
+    def run(@PathVariable levelNumber: Int, @RequestBody code: String): ResponseEntity[java.util.List[String]] = {
+      codeService.compile(code)
       val result = codeService.run(code)
-      pathService.validatePathInLevel(levelNumber, (0,0), result)
-      new ResponseEntity[List[String]](result, HttpStatus.OK)
+      val resPath = pathService.validatePathInLevel(levelNumber, (0,0), result)
+
+      new ResponseEntity[java.util.List[String]](resPath, HttpStatus.OK)
     }
 
     @ExceptionHandler(value = Array(classOf[Exception]))
--- service/src/main/scala/com/mfed/services/impl/CodeServiceImpl.scala
+++ service/src/main/scala/com/mfed/services/impl/CodeServiceImpl.scala
@@ -1,17 +1,18 @@
 package com.mfed.services.impl
 
 import java.io.File
+import java.lang.reflect.Method
 import java.net.{URL, URLClassLoader}
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import javax.tools.{ToolProvider, JavaCompiler}
+import javax.tools.{JavaCompiler, ToolProvider}
 
 import com.mfed.GameMeApplication
 import com.mfed.services.CodeService
 import org.springframework.stereotype.Service
 
 @Service
-class CodeServiceImpl extends CodeService{
+class CodeServiceImpl extends CodeService {
   override def compile(code: String): String = {
     val root: File = prepFile
     val sourceFile: File = new File(root, "Test.java")
@@ -24,11 +25,11 @@ class CodeServiceImpl extends CodeService{
   }
 
   override def run(code: String): List[String] = {
-    val root = prepFile
-    val classLoader: URLClassLoader = URLClassLoader.newInstance(Array[URL](root.toURI.toURL))
-    val cls: Class[_] = Class.forName("Test", true, classLoader)
-    val instance: Any = cls.newInstance
-    List(instance.toString)
+    val classLoader: URLClassLoader = URLClassLoader.newInstance(Array[URL](prepFile.toURI.toURL))
+    val instance: Any = Class.forName("Test", true, classLoader).newInstance
+    val method: Method = instance.getClass.getMethod("run")
+
+    List(String.valueOf(method.invoke(instance)))
   }
 
   def prepFile: File = {
