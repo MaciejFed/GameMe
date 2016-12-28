9b5ef6cc0238ce333b45b2de3ff017fa84ba06bb
magdgar <magdgar@gmail.com>
2016-12-28 23:53:13 +0100

    Update startDev.sh

--- startDev.sh
+++ startDev.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Starting InteliJ..."
-#there insert path to InteliJ eg "/c/Program Files/IntelliJ IDEA 15.0.1/bin"
-cd "fill"
-./idea64.exe &
-
 echo "Starting Mongo server..."
 mongod &
 
@@ -12,6 +7,11 @@ echo "Starting Robomongo..."
 #there insert path to Robomongo eg "/c/Program Files/Robomongo 0.9.0-RC9"
 cd "fill"
 ./Robomongo.exe
+
+echo "Starting InteliJ..."
+#there insert path to InteliJ eg "/c/Program Files/IntelliJ IDEA 15.0.1/bin"
+cd "fill"
+./idea64.exe &
 
 read -p "Press any key..."
 exit 0
