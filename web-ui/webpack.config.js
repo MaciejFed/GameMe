0fe066ad472cbcb2dddd16a236b9b8c38604a24b
MaciejFed <mfedorowiat@gmail.com>
2017-01-14 21:52:06 +0100

    loader fix

--- web-ui/webpack.config.js
+++ web-ui/webpack.config.js
@@ -40,10 +40,10 @@ var config = {
          )}
          ,{
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[hash].[ext]'
+                }
             }
 
         ]
