06ba3928fac6e4b46d1f1e7a93e892f5100bc72a
maciejfed <mfedorowiat@gmail.com>
2017-01-16 23:07:00 +0100

    structure change

---
+++ web-ui/src/scenes/Level/Level.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import CodeRunner from './components/CodeRunner/CodeRunner';
+import Board from './components/Board/Board'
+import GameMap from './components/Board/game_map'
+import MapClient from './../../service/api/map_client';
+import styles from './level.css'
+
+export default class Level extends React.Component{
+    constructor(){
+        super();
+        this.mapClient = new MapClient();
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <div className={styles.level}>
+                <CodeRunner />
+                <Board gameMap={this.state.gameMap}/>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.mapClient.loadMap(this.getLastLevelPlayed(), function (result, status) {
+            this.setState({
+                gameMap: new GameMap(result)
+            });
+        }.bind(this));
+
+    }
+
+    getLastLevelPlayed(){
+        return 125;
+    }
+}
--- web-ui/src/scenes/Level/components/Board/Board.js
+++ web-ui/src/scenes/Level/components/Board/Board.js
@@ -2,64 +2,49 @@ const RECTANGLE_SIZE = window.innerWidth / 20;
 const STAGE_NAME = "gameStage";
 
 import React from 'react';
-import CodeRunner from '../code_runner/code-runner'
-import stageStyles from './stage-style.css'
-import CodeRunnerClient from '../../client/code-runner-client';
-import MapClient from '../../client/map-client';
-import GameObject from './game-object';
+import CodeRunner from '../CodeRunner/CodeRunner'
+import stageStyles from './board.css'
+import CodeRunnerClient from '../../../../service/api/code_runner_client';
+import GameObject from './game_object';
 import Direction from './direction';
-const GameMap = require('./../map_wrapper/map');
+const GameMap = require('./game_map');
 const OPEN_FIELD_COLOR = "#999";
 const CLOSED_FIELD_COLOR = "#222";
 let robot = require("./robot.png");
 
 
-export default class Stage extends React.Component {
+export default class Board extends React.Component {
 
     constructor(props) {
         super(props);
         this.codeRunnerCLient = new CodeRunnerClient();
-        this.mapClient = new MapClient();
         this.stage = null;
         this.levelUrl = null;
         this.robot = new GameObject(new createjs.Shape(), RECTANGLE_SIZE);
         this.directionValues = [];
-        this.state = {};
     }
 
-    componentDidMount() {
-        this.mapClient.loadMap(this.getLastLevelPlayed(), function (result, status) {
-            this.setState({
-                gameMap: new GameMap(result)
-            });
-        }.bind(this));
-
-    }
     componentDidUpdate() {
         this.refreshStage();
     }
 
     render() {
-        if (this.state.gameMap != undefined)
+        if (this.props.gameMap != undefined)
             return (
-                <div className={stageStyles.stage}>
-                    <CodeRunner />
-                    <div id="board" className={stageStyles.board} >
+                <div id="board" className={stageStyles.board} >
                         <canvas id="gameStage"
-                                width={RECTANGLE_SIZE * this.state.gameMap.width}
-                                height={RECTANGLE_SIZE * this.state.gameMap.height}>
+                                width={RECTANGLE_SIZE * this.props.gameMap.width}
+                                height={RECTANGLE_SIZE * this.props.gameMap.height}>
                         </canvas>
                         <button onClick={() => this.animateBall(this.directionValues)} className={stageStyles.animateButton}>Animate</button>
-                    </div>
                 </div>
+
             );
         else
             return (<div></div>)
     }
 
     refreshStage() {
-        if (this.state.gameMap == undefined)
-            return;
         let robotBitmap = new createjs.Bitmap(robot);
         this.robot = new GameObject(robotBitmap, RECTANGLE_SIZE);
 
@@ -82,13 +67,13 @@ export default class Stage extends React.Component {
     }
 
     renderRectangles(){
-        for (let h = 0; h < this.state.gameMap.height; h++) {
-            for (let w = 0; w < this.state.gameMap.width; w++) {
+        for (let h = 0; h < this.props.gameMap.height; h++) {
+            for (let w = 0; w < this.props.gameMap.width; w++) {
                 let border = new createjs.Shape();
                 border.graphics.beginStroke(OPEN_FIELD_COLOR);
                 border.graphics.setStrokeStyle(2);
 
-                if (this.state.gameMap.openField({x: w, y: h}))
+                if (this.props.gameMap.openField({x: w, y: h}))
                     border.graphics.beginFill(CLOSED_FIELD_COLOR);
                 
                 border.graphics.drawRect(0, 0, RECTANGLE_SIZE, RECTANGLE_SIZE);
@@ -109,10 +94,6 @@ export default class Stage extends React.Component {
             }
             createjs.Ticker.setFPS(60);
             createjs.Ticker.addEventListener("tick", this.stage);
-    }
-
-    getLastLevelPlayed(){
-        return 125;
     }
 
 };
