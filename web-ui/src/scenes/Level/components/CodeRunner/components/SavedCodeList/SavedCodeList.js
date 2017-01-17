1f558d440dab72ec05f47eab08f2bef0a6e42728
maciejfed <mfedorowiat@gmail.com>
2017-01-17 20:05:37 +0100

    SaveCodeList miised fix

---
+++ web-ui/src/scenes/Level/components/CodeRunner/components/SavedCodeList/SavedCodeList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './savedcodelist.css';
+
+export default class SavedCodeList extends React.Component{
+    constructor(){
+       super();
+    }
+
+    render(){
+        return(
+            <div className={styles.codeButtons}>
+                <button className={styles.savedCodeButton}/>
+            </div>
+        )
+    }
+}
